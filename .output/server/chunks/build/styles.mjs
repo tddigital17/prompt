83edb5d92685be5183c3aed7cb5487877f1c539b
tddigital17 <tekomdevdigital@gmail.com>
2026-09-09 19:11:23 +0800

    udate lagi

--- .output/server/chunks/build/styles.mjs
+++ .output/server/chunks/build/styles.mjs
@@ -3,12 +3,12 @@ const styles = {
   "node_modules/nuxt/dist/app/entry.js": () => import('./entry-styles.BVbEBsWn.mjs').then(interopDefault),
   "app.vue": () => import('./app-styles.C-z-nYMv.mjs').then(interopDefault),
   "pages/admin/index.vue": () => import('./index-styles.JQcWHx2q.mjs').then(interopDefault),
+  "pages/admin/settings.vue": () => import('./settings-styles.C1ggHCf_.mjs').then(interopDefault),
   "pages/admin/prompts.vue": () => import('./prompts-styles.DrzxZwD8.mjs').then(interopDefault),
   "pages/admin/categories.vue": () => import('./categories-styles.H5SqWmcP.mjs').then(interopDefault),
-  "pages/admin/settings.vue": () => import('./settings-styles.C1ggHCf_.mjs').then(interopDefault),
+  "components/AdminSidebar.vue": () => import('./AdminSidebar-styles.DA3RXZk3.mjs').then(interopDefault),
   "node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.Pd4Strki.mjs').then(interopDefault),
   "node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.BMmh3bAG.mjs').then(interopDefault),
-  "components/AdminSidebar.vue": () => import('./AdminSidebar-styles.DA3RXZk3.mjs').then(interopDefault),
   "components/AdBanner.vue": () => import('./AdBanner-styles.BUZV51TT.mjs').then(interopDefault)
 };
 
